cc0906a6120a7858f84db5bf62844497ea239c2a
Jennivis <n1vije00@students.oamk.fi>
2021-12-12 18:02:21 +0200

    Luotu funktiot startData ja startTest

--- pages/jenspa/matikka.js
+++ pages/jenspa/matikka.js
@@ -25,8 +25,18 @@ let zeroRight = "0/0 Ei yhtään oikein. Kokeilepas uudelleen."
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-
+function startData() {
+    document.getElementById("q1").style.display = "none";
+    document.getElementById("q2").style.display = "none";
+    document.getElementById("q3").style.display = "none";
+    document.getElementById("q4").style.display = "none";
+    document.getElementById("q5").style.display = "none";
+    document.getElementById("finishTest").style.display = "none";
+}
+function startTest() {
+    document.getElementById("startTest").style.display = "none";
+    document.getElementById("q1").style.display = "block";
+}
     // Poistetaan vastauskentistä lukitus
     document.getElementById("answer1").disabled = false;
     // document.getElementById("answer2").disabled = false;
